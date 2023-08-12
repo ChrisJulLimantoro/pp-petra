1b1b1083f1faae4e7bd9dfb495924408aee862e5
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2023-08-12 11:28:06 +0700

    feat: add login oauth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Google_Client;
+
+class AuthController extends Controller
+{
+    private $googleClient;
+    public function __construct() {
+        $this->googleClient = new Google_Client();
+        
+        $this->googleClient->setClientId("224861351803-csdq30ndrfnjpkhdqv5tcmh1r6d7q9af.apps.googleusercontent.com");
+        $this->googleClient->setClientSecret("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");
+        $this->googleClient->setRedirectUri(url("/processLogin"));
+        $this->googleClient->addScope("email");
+        $this->googleClient->addScope("profile");
+    }
+    function loginView(Request $request)
+    {
+        $data['link'] = $this->googleClient->createAuthUrl();
+        return view('login', $data);
+    }
+
+    function login(Request $request) {
+        if ($request->getScheme() == 'http') {
+            if (!isset($request->code)) {
+                return redirect()->to("/login")->with('error', "Error");
+            }
+            $token = $this->googleClient->fetchAccessTokenWithAuthCode($request->code);
+            $payload = $this->googleClient->verifyIdToken($token['id_token']);
+            // dd($payload);
+            if ($payload) {
+                // dd($payload['email']);
+                //check petra mail
+                if(isset($payload['hd']) && str_ends_with($payload['hd'], "petra.ac.id")){
+                    //set session  
+                    $request->session()->put('email', $payload['email']);
+                    $request->session()->put('name', $payload['name']);
+                    // session untuk mahasiswa tanpa nrp maka dosen dan admin
+                    if(str_ends_with($payload['hd'], "john.petra.ac.id")){
+                        $request->session()->put('nrp',substr($payload['email'],0,9));
+                    }
+                    return  redirect()->to("/");
+                    // return;
+
+                }else{
+                    echo 'gagal salah email, bbkn email petra';
+                    // return redirect()->to("/login")->with('error', "Please Use Your @john.petra.ac.id email");
+                }
+
+
+            } else {
+                // Invalid ID token
+            }
+            return;
+        }
+
+        if ($request->g_csrf_token   != null) {
+            // valid CSRF token
+            // Handle the error here
+
+            \Firebase\JWT\JWT::$leeway = 60;
+
+            do {
+                $attempt = 0;
+                try {
+                    //get the cretential from post sent by google
+                    $id_token = $request->credential;
+
+                    //verify the idtoken to convert to the data
+                    $payload = $this->googleClient->verifyIdToken($id_token);
+            
+                    if ($payload) {
+                        // dd($payload);
+
+                        //check petra mail
+                        if(isset($payload['hd']) && $payload['hd'] == "petra.ac.id"){
+                            //set session  
+                            
+                            //with ajax
+                            session()->put('email', $payload['email']);
+                            echo 'berhasil';
+                            // return;
+
+
+                        }else{
+                            echo 'gagal salah email, bbkn email petra';
+                            // return redirect()->to("/login")->with('error', "Please Use Your @john.petra.ac.id email");
+                        }
+
+
+                    } else {
+                        // Invalid ID token
+                    }
+
+                    $retry = false;
+
+                } catch (\Firebase\JWT\BeforeValidException $e) {
+                    $attempt++;
+                    $retry = $attempt < 2;
+                }
+            } while ($retry);
+            
+        } else {
+            return redirect()->to("/login")->with('error', "Error CSRF");
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [AuthController::class, 'loginView']);
+Route::get('/processLogin', [AuthController::class, 'login']);
