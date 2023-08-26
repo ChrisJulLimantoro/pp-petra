427db6760cccd5f92581565cffb2ed86312b2ed2
Nico <nicos.t.65@gmail.com>
2023-08-27 00:06:26 +0700

    chore: change tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+content: ["./src/**/*.{js,jsx,ts,tsx}"],
+theme: {
+    extend: {},
+},
+plugins: [],
+});
+
 export default {
+ 
+
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
