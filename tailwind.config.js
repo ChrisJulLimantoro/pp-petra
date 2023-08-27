b1cf3c5db7655bff15fe67fe520dcdd57b801d46
Nico <nicos.t.65@gmail.com>
2023-08-27 20:32:26 +0700

    chore: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,22 @@ import forms from '@tailwindcss/forms';
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
+    content: [
+                './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+                './storage/framework/views/*.php',
+                './resources/views/**/*.blade.php',
+                './resources/js/**/*.jsx',
+            ],
+        
+            theme: {
+                extend: {
+                    fontFamily: {
+                        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                    },
+                },
+            },
+        
+            plugins: [forms],
 });
 // export default {
  
