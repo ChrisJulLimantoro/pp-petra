045f23a29cb98a3409fd8af27670206fa4b1158c
Nico <nicos.t.65@gmail.com>
2023-10-19 17:31:49 +0700

    refactor: use context instead of props

--- resources/js/Components/DataTable/TableFooter.jsx
+++ resources/js/Components/DataTable/TableFooter.jsx
@@ -1,35 +1,51 @@
 import { CardFooter, Typography, IconButton } from "@material-tailwind/react";
 import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/24/outline";
+import { DataTableContext } from "./DataTable";
+import { useContext } from "react";
 
-export default function TableFooter ({ children, currentPage, perPage, totalPages, totalData, prev, next, className = '' }) {
+export default function TableFooter ({ children, className = '' }) {
+    const context = useContext(DataTableContext)
+    
     return (
-        <CardFooter className={"flex items-center justify-between border-t border-blue-gray-50 p-4 " + className}>
-                <div className="flex items-center">
-                    Showing {(currentPage-1) * perPage + 1} to {currentPage * perPage > totalData ? totalData : currentPage * perPage} of {totalData} entries
-                </div>
-                <div className="flex items-center gap-8">
-                    <IconButton
-                        size="sm"
-                        variant="outlined"
-                        onClick={prev}
-                        disabled={currentPage === 1}
-                    >
-                        <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" />
-                    </IconButton>
-                    <Typography color="gray" className="font-normal">
-                        Page <strong className="text-gray-900">{currentPage}</strong> of{" "}
-                        <strong className="text-gray-900">{totalPages}</strong>
-                    </Typography>
-                    <IconButton
-                        size="sm"
-                        variant="outlined"
-                        onClick={next}
-                        disabled={currentPage === totalPages}
-                    >
-                        <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
-                    </IconButton>
-                </div>
-                {children}
+        <CardFooter 
+            className={"flex flex-col md:flex-row items-center justify-between border-t border-blue-gray-50 p-4 gap-5" + className}
+        >
+            {children}
+            
+            <div className="flex items-center">
+                {
+                    "Showing " + ((context.currentPage-1) * context.perPage + 1) + " to " + 
+                    (
+                        context.currentPage * context.perPage > context.filteredData.length ? 
+                        context.filteredData.length : 
+                        context.currentPage * context.perPage
+                    ) + 
+                    " of " + 
+                    context.filteredData.length + " entries"
+                } 
+            </div>
+            <div className="flex items-center gap-8">
+                <IconButton
+                    size="sm"
+                    variant="outlined"
+                    onClick={context.prevPage}
+                    disabled={context.currentPage === 1}
+                >
+                    <ArrowLeftIcon strokeWidth={2} className="h-4 w-4" />
+                </IconButton>
+                <Typography color="gray" className="font-normal">
+                    Page <strong className="text-gray-900">{context.currentPage}</strong> of{" "}
+                    <strong className="text-gray-900">{context.totalPages}</strong>
+                </Typography>
+                <IconButton
+                    size="sm"
+                    variant="outlined"
+                    onClick={context.nextPage}
+                    disabled={context.currentPage === context.totalPages}
+                >
+                    <ArrowRightIcon strokeWidth={2} className="h-4 w-4" />
+                </IconButton>
+            </div>
         </CardFooter>
     );
 }
--- resources/js/Components/DataTable/TableHeader.jsx
+++ resources/js/Components/DataTable/TableHeader.jsx
@@ -1,18 +1,32 @@
 import { Select, Option, Input, Typography } from "@material-tailwind/react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Head } from "@inertiajs/react";
+import { useContext } from "react";
+import { DataTableContext } from "./DataTable";
 
-export default function TableHeader ({ title, description = '', actionBtn, perPage, searchData, changePerPage, children, className = '' }) {
+export default function TableHeader ({ title, description = '', children, className = '' }) {
+    const context = useContext(DataTableContext);
+
     return (
         <>
             <Head>
                 <style>
                 {`
-                    div:has(> button.numItemSelect) {
-                        min-width: 5rem !important;
+                    @media screen and (min-width:768px) {
+                        div:has(> button.numItemSelect) {
+                            min-width: 5rem !important;
+                        }
+                        .numItemSelect, .numItemSelect + label {
+                            max-width: 5rem;
+                        }
                     }
-                    .numItemSelect, .numItemSelect + label {
-                        max-width: 5rem;
+                    @media screen and (max-width:767px) {
+                        div:has(> button.numItemSelect) {
+                            min-width: 4rem !important;
+                        }
+                        .numItemSelect, .numItemSelect + label {
+                            max-width: 4rem;
+                        }
                     }
                 `}
                 </style>
@@ -20,37 +34,40 @@ export default function TableHeader ({ title, description = '', actionBtn, perPa
             
             <div className={className}>
                 {/* Title, Description, Action Buttons */}
-                <div className="mb-5 px-5 py-3 flex items-center justify-between gap-8">
+                <div className="md:mb-2 px-5 py-3 flex items-center justify-between md:gap-8">
                     <div>
                         <Typography variant="h5" color="blue-gray">
-                        {title}
+                            {title}
                         </Typography>
                         <Typography color="gray" className="mt-1 font-normal">
-                        {description}
+                            {description}
                         </Typography>
                     </div>
-                    {actionBtn}
+                    
                 </div>
 
                 {/* Search */}
-                <div className="flex flex-col items-center justify-between gap-4 md:flex-row px-4">
+                <div className="w-full flex flex-col items-start justify-between gap-4 md:flex-row px-4">
                     {children}
+
                     <div className="flex items-center justfiy-start gap-4">
                         <Select
-                            value={perPage}
-                            onChange = {(e) => changePerPage(e)}
+                            value={context.perPage.toString()}
+                            onChange = {(e) => context.changePerPage(e)}
                             className="relative z-99 numItemSelect"
                         >
                             <Option key="10" value="10">10</Option>
                             <Option key="25" value="25">25</Option>
                             <Option key="50" value="50">50</Option>
                             <Option key="100" value="100">100</Option>
-                            <Option key="semua" value="All">All</Option>
+                            <Option key="semua" value="All">All</Option>    
                         </Select>
+
                         <Input
                             label="Search"
                             icon={<MagnifyingGlassIcon className="h-5 w-5" />}
-                            onChange={(e) => searchData(e.target.value)}
+                            containerProps={{ className:'min-w-[100px] md:min-w-fit' }}
+                            onChange={(e) => context.searchData(e.target.value)}
                         />
                     </div>
                 </div>
