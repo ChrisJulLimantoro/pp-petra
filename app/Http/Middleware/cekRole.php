4777f02a2a119a29ad9d2cb264b1e3916f54b79c
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2023-11-13 10:20:35 +0700

    fix : middleware logic

--- app/Http/Middleware/cekRole.php
+++ app/Http/Middleware/cekRole.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Symfony\Component\HttpFoundation\Response;
 
 class cekRole
@@ -13,13 +14,15 @@ class cekRole
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next, $access): Response
+    public function handle(Request $request, Closure $next): Response
     {
-        foreach(explode(",",$access) as $role){
-            if(in_array($role, $request->session()->get('roles'))){
-                return $next($request);
-            }
+        $name = $request->route()->getName();
+        $method = $request->getMethod();
+        $cek = json_decode(Http::withToken(session('token'))->post(env('API_URL').'/rbac/cekRole',['route'=>$name,'method'=>$method,'user_id'=>session('user_id')]))->data;
+        if($cek){
+            return $next($request);
+        }else{
+            return redirect()->to(route('dashboard'))->with('error', 'You are not authorized to access this page');
         }
-        return redirect()->to(route('dashboard'))->with('error', 'You are not authorized to access this page');
     }
 }
