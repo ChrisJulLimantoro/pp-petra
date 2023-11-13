b4389164c08cc37f088416e8d6f9e367d50e4e00
ChrisJulLimantoro <christopherlimantoro@gmail.com>
2023-11-13 07:53:10 +0700

    fix: web.php

--- routes/web.php
+++ routes/web.php
@@ -51,7 +51,7 @@ Route::get('test',function(){
     return 'test';
 })->middleware('cekRole:admin');
 Route::get("/viewMahasiswa", [BulkInsertStudentController::class, 'index'])->name('viewMahasiswa');
-Route::post("/uploadMahasiswa", [BulkInsertStudentController::class, 'insertSchedule'])->name('uploadMahasiswa');
+Route::post("/uploadMahasiswa", [BulkInsertStudentController::class, 'insert'])->name('uploadMahasiswa');
 
 Route::get('/routes', [App\Http\Controllers\RBACController::class, 'getAllRoutes'])->name('routes');
 Route::get("/", [AuthController::class, 'loginView'])->name('login');
